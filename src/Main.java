2f88a284eed435ef46dd4b821ebfca66a7975f0f
Dara Ajiboye <ota9@duke.edu>
2025-04-21 00:30:10 -0400

    Interface Set-Up, Pokemon.java Set-Up, Added PokemonQueryImpl

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        PokemonQueryImpl query = new PokemonQueryImpl();
+        try {
+            int count = query.loadDataset("data/pokemon.csv");
+            System.out.println("Successfully loaded " + count + " Pokémon!");
+        } catch (IOException e) {
+            System.out.println("Error loading dataset: " + e.getMessage());
+        }
     }
 }
