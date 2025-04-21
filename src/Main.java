549ff21c05bf7f614a32f81167559ba738f4c5aa
Dara Ajiboye <ota9@duke.edu>
2025-04-21 00:46:36 -0400

    Fixed Filepath that loads everything to project

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         PokemonQueryImpl query = new PokemonQueryImpl();
         try {
             int count = query.loadDataset("data/pokemon_data.csv");
+            //this loads all Pokémon into the project - Dara
             System.out.println("Successfully loaded " + count + " Pokémon!");
         } catch (IOException e) {
             System.out.println("Error loading dataset: " + e.getMessage());
